a9c55265fa1d30c8c3325114259b9ae3b4183dc5
apiwat <bunderscorest@gmail.com>
2024-06-24 11:14:14 +0700

    edit-conter

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -6,47 +6,31 @@ const Content = () => {
   return (
     <div className = {`flex flex-col justify-start mt-[80px] ${isPC ? 'min-w-[80vw]  ' : 'min-w-[100vw]  '} dm-sans-text`}>
       {isPC?(
-        
-          <div className=' บน  flex justify-around'> 
-            <div className=' w-[600px] h-[400px] flex justify-center items-center relative'>
-              <div className=' w-[380px] h-[380px] rounded-full bg-[#002B5A] flex justify-center items-center absolute  '>
-                <img src="/profile.png" alt="profile-image" className=' h-[280px] w-[280px] rounded-full' />
-              </div>
-            </div>
-
-            <div className=' w-[600px] h-[400px] flex flex-col rounded-lg justify-center items-end font-black text-[35px] overflow-clip '>
-              <div className=' w-[500px] text-[#FFF5E1]'>
-                <h1>Hey, I'm bestawIpa</h1>
-                <p>I like to craft solid frontend product, turning ideas into real life products is my calling.</p>
-              </div>
-            </div>
+        <div className=' บน  flex justify-around'> 
+          <div className=' w-[600px] h-[400px] flex justify-center items-center relative'>
+            <div className=' w-[380px] h-[380px] rounded-full bg-[#002B5A] flex justify-center items-center absolute  '>
+              <img src="/profile.png" alt="profile-image" className=' h-[280px] w-[280px] rounded-full' />
+            </div>            
           </div>
 
-      
+          <div className=' w-[600px] h-[400px] flex flex-col rounded-lg justify-center items-end font-black text-[35px] overflow-clip '>
+            <div className=' w-[500px] text-[#FFF5E1]'>
+              <h1>Hey, I'm bestawIpa</h1>
+              <p>I like to craft solid frontend product, turning ideas into real life products is my calling.</p>
+            </div>
+          </div>
+        </div>
        
       ):(
-        // <div className='flex flex-col  min-w[80%vw] bg-white'> 
-        //     <div className=' w-[600px] h-[400px] flex justify-center items-center relative'>
-        //       <div className=' w-[380px] h-[380px] rounded-full bg-[#002B5A] flex justify-center items-center absolute  '>
-        //         <img src="/profile.png" alt="profile-image" className=' h-[280px] w-[280px] rounded-full' />
-        //       </div>
-        //     </div>
 
-        //     <div className=' w-[600px] h-[400px] flex flex-col rounded-lg justify-center items-end font-black text-[35px] overflow-clip '>
-        //       <div className=' w-[500px] text-[#FFF5E1]'>
-        //         <h1>Hey, I'm bestawIpa</h1>
-        //         <p>I like to craft solid frontend product, turning ideas into real life products is my calling.</p>
-        //       </div>
-        //     </div>
-        //   </div>
-        <div className=' min-w[80%vw] '> 
-          <div className=' mt-[40px]  flex flex-col items-center '>
+        <div className=' min-w[80%vw] max-h-[1290px] '> 
+          <div className=' mt-[10px] h-full   flex flex-col items-center '>
             <div className=' w-[300px] h-[300px] rounded-full bg-[#002B5A] flex justify-center items-center'>
               <img src="/profile.png" alt="" className=' w-[250px] h-[250px] rounded-full'/>
             </div>
 
-            <div className=' mt-[20px] w-[230px] flex justify-center text-[#FFF5E1] font-black text-[18px]'>
-              <div>
+            <div className=' mt-[10px] w-[300px] min-h-[200px] flex justify-center items-center font-black text-[18px]'>
+              <div className='  w-full h-full ml-4 text-[#F6E6CB]'>
                 <h1>Hey, I'm bestawIpa</h1>
                 <p>I like to craft solid frontend product, turning ideas into real life products is my calling.</p>
               </div>
@@ -54,8 +38,6 @@ const Content = () => {
           </div>
         </div>
       )}
-      
-
 
     </div>
   )
